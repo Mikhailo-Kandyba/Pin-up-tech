a1312a21950023bd6efa992c448efd02dffebe95
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-07-20 18:04:33 +0300

    bugs fixing

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-// $(document).ready(function(){
-//     var $moveable = $('.move-rect');
-//     var rectPosY = parseInt($('.move-rect').css('top'), 10);
-//     var rectPosX = parseInt($('.move-rect').css('right'), 10);
-//     $(".main").mousemove(function(e){
-//         $moveable.css({'top': rectPosY-e.pageY/40,'right': rectPosX-e.pageX/40});
-//     });
-// });
+$(document).ready(function(){
+    var $moveable = $('.move-rect');
+    var rectPosY = parseInt($('.move-rect').css('top'), 10);
+    var rectPosX = parseInt($('.move-rect').css('right'), 10);
+    $(".main").mousemove(function(e){
+        $moveable.css({'top': rectPosY-e.pageY/40,'right': rectPosX-e.pageX/40});
+    });
+});
