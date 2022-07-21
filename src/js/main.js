70acaf78e53112fdf9bd463ddd5297b2ecc35076
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-07-21 22:45:18 +0300

    latest changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    var $moveable = $('.move-rect');
-    var rectPosY = parseInt($('.move-rect').css('top'), 10);
-    var rectPosX = parseInt($('.move-rect').css('right'), 10);
-    $(".main").mousemove(function(e){
-        $moveable.css({'top': rectPosY-e.pageY/40,'right': rectPosX-e.pageX/40});
+    let $moveable = $('.move-rect');
+    // var rectPosY = parseInt($('.move-rect').css('left'), 10);
+    let rectPosX = parseInt($('.move-rect').css('right'), 10);
+    $(".page-content").mousemove(function(e){
+        $moveable.css({'right': rectPosX-e.pageX/10});
     });
 });
