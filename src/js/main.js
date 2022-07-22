a1b00f81f9ddd11447d77480e3730b841d48bdee
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-07-22 11:43:22 +0300

    update share

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-// $(document).ready(function(){
-//     let $moveable = $('.move-rect');
-//     // var rectPosY = parseInt($('.move-rect').css('left'), 10);
-//     let rectPosX = parseInt($('.move-rect').css('right'), 10);
-//     $(".page-content").mousemove(function(e){
-//         $moveable.css({'right': rectPosX-e.pageX/10});
-//     });
-// });
+$(document).ready(function(){
+    let $moveable = $('.move-rect');
+    // var rectPosY = parseInt($('.move-rect').css('left'), 10);
+    let rectPosX = parseInt($('.move-rect').css('right'), 10);
+    $(".page-content").mousemove(function(e){
+        $moveable.css({'right': rectPosX-e.pageX/10});
+    });
+});
